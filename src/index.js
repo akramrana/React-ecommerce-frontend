e7fff258c190c59dd9711e7169c9f60cb93a3532
Akram Hossain <akram686202@gmail.com>
2020-08-03 14:36:16 +0600

    feature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'pace-js';
+import 'pace-js/themes/orange/pace-theme-minimal.css';
 import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Link, BrowserRouter as Router, Switch} from 'react-router-dom';
-import Pace from 'react-pace-progress';
 import Home from './components/Home';
 const Cms = React.lazy(() => import('./components/Cms'));
 const Products = React.lazy(() => import('./components/Products'));
@@ -22,7 +23,6 @@ const ChangePassword = React.lazy(() => import('./components/ChangePassword'));
 const ForgotPassword = React.lazy(() => import('./components/ForgotPassword'));
 const Wishlist = React.lazy(() => import('./components/Wishlist'));
 const Notfound = React.lazy(() => import('./components/Notfound'));
-const paceColor = "#27ae60";
 
 //window.jQuery = $;
 //window.$ = $;
@@ -35,139 +35,139 @@ const routing = (
           <Route
             path="/register"
             render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <Registration {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/home" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <Home {...props}/>
               </Suspense>
             )}/>
 	      <Route path="/page/:id/:title" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <Cms {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/products/:type/:id/:title" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <Products {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/products/:type/:id/:title" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <Products {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/products/:type/:id/:title" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <Products {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/product/:id/:title" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <ProductDetails {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/products/all-category" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <Products {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/login" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <Login {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/logout" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <Logout {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/cart" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <Cart {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/checkout" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <Checkout {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/order-details/:id" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <OrderDetails {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/myaccount" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <MyAccount {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/addresses" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <MyAddress {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/profile" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <Profile {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/change-password"  
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <ChangePassword />
               </Suspense>
             )}
           />
 	      <Route path="/products/search" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <Products {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/forgot-password" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <ForgotPassword {...props}/>
               </Suspense>
             )}
           />
 	      <Route path="/wishlist" 
           render={(props) => (
-              <Suspense fallback={<Pace color={paceColor} number={5}/>}>
+              <Suspense fallback={<p align="center">Loading...</p>}>
                 <Wishlist {...props}/>
               </Suspense>
             )}
